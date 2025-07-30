6d36b93e22714941c455ff4aa949dd3b69793e78
sharma-s7 <shubhamsharmadev9@gmail.com>
2025-07-30 11:56:14 -0400

    cal area

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -3,8 +3,9 @@ let width;
 
 function calculateArea() {
     length = parseFloat(document.getElementById('length').value);
-    width = parseFloat(documentgetElementbyId('width').value);
+    width = parseFloat(document.getElementById('width').value);
 
     let area = length * width;
+
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
