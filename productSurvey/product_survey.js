d32d72c291b873ed55943a5518c6e282a8a159e0
sharma-s7 <shubhamsharmadev9@gmail.com>
2025-08-01 11:18:48 -0400

    addded new textarea

--- productSurvey/product_survey.js
+++ productSurvey/product_survey.js
@@ -6,9 +6,9 @@ function submitFeedback () {
     const designation = document.getElementById('designation').value;
     const productType = document.getElementById('productType').value;
     const feedback = document.getElementById('feedbackText').value;
-   
+    const experience = document.getElementById('feedbackExperience').value;
     alert('Thank you for your valuable feedback')
-    
+
     document.getElementById('userName').innerHTML = username;
     document.getElementById('userAge').innerHTML = age;
     document.getElementById('userEmail').innerHTML = email;
@@ -16,6 +16,7 @@ function submitFeedback () {
     document.getElementById('userDesignation').innerHTML = designation;
     document.getElementById('userProductChoice').innerHTML = productType;
     document.getElementById('userFeedback').innerHTML = feedback;
+    document.getElementById('userExperience').innerHTML = experience;
     document.getElementById('userInfo').style.display = 'block';
 }
   const submitButton = document.getElementById('submitBtn');
